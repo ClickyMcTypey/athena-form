e87a6dc9b16a8165bb973eef809a68932b384fe4
ClickyMcTypey <reyrandel@gmail.com>
2026-05-08 07:54:31 +0800

    Start

---
+++ src/config/form.config.js
@@ -0,0 +1,55 @@
+// src/config/form.config.js
+
+export const FORM_CONFIG = {
+  hubspot: {
+    region: "na1",
+    portalId: "20122740",
+    formId: "a7a1a71d-fee0-4c1b-85ea-05aede171179",
+    submitBaseUrl: "https://api.hsforms.com/submissions/v3/integration/submit",
+  },
+
+  chili: {
+    tenant: "athena",
+    router: "commercial-round-robin",
+    formIds: ["athn_form"],
+  },
+
+  fallbackValue: "athena20122740",
+
+  animationTime: 300,
+
+  excludedAttributionSteps: [
+    "loading",
+    "email",
+    "info",
+    "loading_chili",
+    "calendar",
+    "success",
+    "error",
+    "closed",
+  ],
+
+  progressSteps: [
+    "9",
+    "loading",
+    "email",
+    "info",
+    "calendar",
+    "success",
+  ],
+
+  bannedCountries: [
+    "ph",
+    "vn",
+    "id",
+    "in",
+    "ng",
+    "my",
+    "ke",
+    "gt",
+  ],
+
+  redirectUrls: {
+    bannedCountry: "https://jobs.athena.com",
+  },
+};
