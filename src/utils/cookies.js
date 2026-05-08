f8695effc8cd7140a5b7c958d784c9063d9c3f59
ClickyMcTypey <reyrandel@gmail.com>
2026-05-08 18:24:57 +0800

    added cookie utils

---
+++ src/utils/cookies.js
@@ -0,0 +1,30 @@
+// src/utils/cookies.js
+
+export function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+
+  if (parts.length === 2) {
+    return parts.pop().split(";").shift();
+  }
+
+  return "";
+}
+
+export function parseEncodedCookiePairs(cookieValue) {
+  if (!cookieValue) return {};
+
+  const output = {};
+
+  const decodedCookie = decodeURIComponent(cookieValue);
+
+  decodedCookie.split(";").forEach((pair) => {
+    const [key, value] = pair.split("=");
+
+    if (key && value) {
+      output[key.trim()] = encodeURIComponent(value.trim());
+    }
+  });
+
+  return output;
+}
