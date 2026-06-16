3f37e19e5db1c4d101eee07fa8bc9818c1699772
ClickyMcTypey <reyrandel@gmail.com>
2026-06-17 05:23:11 +0800

    updated cookie helper

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -3,12 +3,10 @@
 export function getCookie(name) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
-
   if (parts.length === 2) {
-    return parts.pop().split(";").shift();
+    return decodeURIComponent(parts.pop().split(";").shift() || "");
   }
-
-  return "";
+  return null;
 }
 
 export function parseEncodedCookiePairs(cookieValue) {
